3022162dc8d76ec8529a0fd540e82be7ddfdbf58
loganschool <logan.cantin@ocsbstudent.ca>
2019-05-28 12:44:35 -0400

    2d array

---
+++ 2dArraysLoganC/2dArraysLoganC/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _2dArraysLoganC
+{
+    partial class frm2darr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudX = new System.Windows.Forms.NumericUpDown();
+            this.nudY = new System.Windows.Forms.NumericUpDown();
+            this.lblx = new System.Windows.Forms.Label();
+            this.lbly = new System.Windows.Forms.Label();
+            this.tbxDisplay = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblavg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudX
+            // 
+            this.nudX.Location = new System.Drawing.Point(205, 55);
+            this.nudX.Name = "nudX";
+            this.nudX.Size = new System.Drawing.Size(120, 20);
+            this.nudX.TabIndex = 0;
+            // 
+            // nudY
+            // 
+            this.nudY.Location = new System.Drawing.Point(249, 91);
+            this.nudY.Name = "nudY";
+            this.nudY.Size = new System.Drawing.Size(120, 20);
+            this.nudY.TabIndex = 1;
+            // 
+            // lblx
+            // 
+            this.lblx.AutoSize = true;
+            this.lblx.Location = new System.Drawing.Point(124, 55);
+            this.lblx.Name = "lblx";
+            this.lblx.Size = new System.Drawing.Size(33, 13);
+            this.lblx.TabIndex = 2;
+            this.lblx.Text = "x size";
+            // 
+            // lbly
+            // 
+            this.lbly.AutoSize = true;
+            this.lbly.Location = new System.Drawing.Point(127, 91);
+            this.lbly.Name = "lbly";
+            this.lbly.Size = new System.Drawing.Size(33, 13);
+            this.lbly.TabIndex = 3;
+            this.lbly.Text = "y size";
+            // 
+            // tbxDisplay
+            // 
+            this.tbxDisplay.Location = new System.Drawing.Point(104, 138);
+            this.tbxDisplay.Multiline = true;
+            this.tbxDisplay.Name = "tbxDisplay";
+            this.tbxDisplay.Size = new System.Drawing.Size(386, 180);
+            this.tbxDisplay.TabIndex = 4;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(409, 51);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(147, 60);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblavg
+            // 
+            this.lblavg.AutoSize = true;
+            this.lblavg.Location = new System.Drawing.Point(205, 337);
+            this.lblavg.Name = "lblavg";
+            this.lblavg.Size = new System.Drawing.Size(35, 13);
+            this.lblavg.TabIndex = 6;
+            this.lblavg.Text = "label1";
+            // 
+            // frm2darr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 362);
+            this.Controls.Add(this.lblavg);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.tbxDisplay);
+            this.Controls.Add(this.lbly);
+            this.Controls.Add(this.lblx);
+            this.Controls.Add(this.nudY);
+            this.Controls.Add(this.nudX);
+            this.Name = "frm2darr";
+            this.Text = "2D Array Logan C";
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudX;
+        private System.Windows.Forms.NumericUpDown nudY;
+        private System.Windows.Forms.Label lblx;
+        private System.Windows.Forms.Label lbly;
+        private System.Windows.Forms.TextBox tbxDisplay;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblavg;
+    }
+}
+
---
+++ 2dArraysLoganC/2dArraysLoganC/Form1.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * Created by: First Last
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - 2D Arrays
+ * This program generates a 2d array and calculates the a
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2dArraysLoganC
+{
+    public partial class frm2darr : Form
+    {
+        // Random number generator
+        Random rand = new Random();
+
+        public frm2darr()
+        {
+            InitializeComponent();
+        }
+
+        // finds the sum of the matrix
+        private double arrayaverage(int[,] arr)
+        {
+            int sum = 0, num = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+                num++;
+            }
+
+            return (double)sum/(double)num;
+        }
+
+        
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Clear text box
+            tbxDisplay.Clear();
+
+            // Get input
+            int xSize = (int)nudX.Value;
+            int ySize = (int)nudY.Value;
+
+            // Create new array of specified size
+            int[,] array = new int[xSize, ySize];
+
+            // Iterate through each item
+            for (int i = 0; i < ySize; i++)
+            {
+                for (int j = 0; j < xSize; j++)
+                {
+                    // Generate a random number
+                    int r = rand.Next(0, 10);
+                    array[j,i] = r; // set it in the array
+                    tbxDisplay.Text += Convert.ToString(r) + " "; // add it to the display
+                }
+
+                tbxDisplay.Text += Environment.NewLine; // Newline
+            }
+
+            // Updating average
+            lblavg.Text = "Average is: " + Convert.ToString(arrayaverage(array));
+        }
+    }
+}
