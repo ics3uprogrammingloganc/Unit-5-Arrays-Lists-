fef58bac7f3aeb72bfafb72bbceef85a2b8d54ea
loganschool <logan.cantin@ocsbstudent.ca>
2019-05-29 13:10:16 -0400

    Lists

---
+++ Lists/Lists/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lists
+{
+    partial class frmlists
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.lblMark = new System.Windows.Forms.Label();
+            this.nudMark = new System.Windows.Forms.NumericUpDown();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMark)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(44, 103);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(274, 303);
+            this.lstMarks.TabIndex = 0;
+            // 
+            // lblMark
+            // 
+            this.lblMark.AutoSize = true;
+            this.lblMark.Location = new System.Drawing.Point(53, 50);
+            this.lblMark.Name = "lblMark";
+            this.lblMark.Size = new System.Drawing.Size(34, 13);
+            this.lblMark.TabIndex = 1;
+            this.lblMark.Text = "Mark:";
+            // 
+            // nudMark
+            // 
+            this.nudMark.Location = new System.Drawing.Point(103, 48);
+            this.nudMark.Name = "nudMark";
+            this.nudMark.Size = new System.Drawing.Size(63, 20);
+            this.nudMark.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(203, 45);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Add Mark";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(130, 431);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(50, 13);
+            this.lblAverage.TabIndex = 4;
+            this.lblAverage.Text = "Average:";
+            // 
+            // frmlists
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 500);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.nudMark);
+            this.Controls.Add(this.lblMark);
+            this.Controls.Add(this.lstMarks);
+            this.Name = "frmlists";
+            this.Text = "Lists Logan C";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMark)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Label lblMark;
+        private System.Windows.Forms.NumericUpDown nudMark;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ Lists/Lists/Form1.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * Created by: Logan Cantin 
+ * Created on: May 29th, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - Lists
+ * This program finds the average of a list of numbers
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lists
+{
+    public partial class frmlists : Form
+    {
+        // List of marks
+        List<int> marks = new List<int>();
+
+        public frmlists()
+        {
+            InitializeComponent();
+        }
+
+        private double FindAverage(ref List<int> inputMarks)
+        {
+            // Sum and count variables
+            int sum = 0, count = 0;
+
+            // Adding each number to the sum and increasing the count
+            foreach (int i in inputMarks)
+            {
+                sum += i;
+                count++;
+            }
+
+            // Return the average
+            return (double)sum / (double)count;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Getting the mark from the user
+            int currentMark = (int)nudMark.Value;
+
+            // Making sure that the mark is between 0-100
+            if (currentMark < 101 && currentMark > -1)
+            {
+                // Add the mark to this listbox
+                lstMarks.Items.Add(currentMark);
+
+                // Find the average
+                double average = FindAverage(ref marks);
+                
+
+                // Update the sum
+                lblAverage.Text = "Average: " + Convert.ToString(average);
+            }
+            else
+            {
+                // Alert the user that their number is not valid
+                MessageBox.Show("Please enter a number from 0 to 100");
+            }
+        }
+    }
+}
